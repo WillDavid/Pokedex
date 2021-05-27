9632d216219b32cef6eec32b70c73590ab1f61fc
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-05-27 01:38:17 -0400

    update app.js

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
+import api from './services/api';
 
 export default function App(){
 
@@ -9,11 +10,49 @@ export default function App(){
 	const [isLoading, setIsLoading] = useState(false);
 
 	// Como teremos um elemento de input, é necessário criarmos uma função para tratarmos deste elemento.
-	
+	/*
+	useEffect( () => {
+
+        async function funcaoLegal(){
+            const response = await api.get(`/pokemon/${typedPokemon}`)
+            setPokemon(response.data);
+        }
+
+        funcaoLegal();
+    }, [])
+	*/
 	function changeInput(event) {
 		setTypedPokemon(event.target.value);
 		console.log(typedPokemon)
 	}
+	
+	async function handleSubmit(event) {
+		event.preventDefault();
+
+		if(!typedPokemon){
+			console.log("Tem nada")
+			return;
+		}
+		setIsLoading(true);
+
+
+		try {
+			const response = await api.get(`/pokemon/${typedPokemon}`)
+			setError(null);
+			setIsLoading(false)
+			setPokemon(response.data);
+			console.log(response.data)
+
+		} catch (error) {
+			setPokemon(null);
+			setIsLoading(false)
+			setError("Pokemon nao encontrado")
+		}
+	}
+
+	
+
+
 	return(
 		<div>
 			<form onSubmit={handleSubmit}>
@@ -24,6 +63,9 @@ export default function App(){
 					(<span>Buscar</span>)}
 				</button>
 			</form>
+
+
+			
 		</div>
 	)
 }
