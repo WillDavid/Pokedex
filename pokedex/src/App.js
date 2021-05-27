6f5fc3f80475e6a5b224ae44a3f9728104dd7626
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-05-27 01:01:23 -0400

    create api,js with axios

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App(){
+
+
+	const [pokemon, setPokemon] = useState(null);
+	const [error, setError] = useState(null);
+	const [typedPokemon, setTypedPokemon] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+	// Como teremos um elemento de input, é necessário criarmos uma função para tratarmos deste elemento.
+	
+	function changeInput(event) {
+		setTypedPokemon(event.target.value);
+		console.log(typedPokemon)
+	}
 	return(
 		<div>
-			<h2>Olá, mundo!</h2>
+			<form onSubmit={handleSubmit}>
+				<input value={typedPokemon} onChange={changeInput}/>
+
+				<button type="submit">
+					{isLoading ? (<span>Carregando</span>) :
+					(<span>Buscar</span>)}
+				</button>
+			</form>
 		</div>
 	)
 }
